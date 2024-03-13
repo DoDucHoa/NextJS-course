415682005ed8fadcdd5e0ca38b4b8bc2bd53e37a
Đỗ Đức Hoà <ddhoa@netcompany.com>
2024-03-13 08:19:42 +0700

    Update tsconfig.json and webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'production',
   entry: './app/page.tsx',
   output: {
     path: path.resolve(__dirname, 'dist'),
