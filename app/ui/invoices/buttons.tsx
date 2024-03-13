3f7036b6ba6d1fbcd8cddc7c04f2ea0a68067efc
Do Duc Hoa <hhoa0978@gmail.com>
2024-03-13 20:24:30 +0700

    Update .gitignore and invoice buttons

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -13,7 +13,7 @@ export function CreateInvoice() {
   );
 }
 
-export function UpdateInvoice() {
+export function UpdateInvoice({ id }: { id: string }) {
   return (
     <Link
       href="/dashboard/invoices"
@@ -24,7 +24,7 @@ export function UpdateInvoice() {
   );
 }
 
-export function DeleteInvoice() {
+export function DeleteInvoice({ id }: { id: string }) {
   return (
     <>
       <button className="rounded-md border p-2 hover:bg-gray-100">
